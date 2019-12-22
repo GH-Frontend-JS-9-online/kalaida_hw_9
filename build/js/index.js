1d42b7df8d9d70e1fb274d95223c7c0ed6048bf0
VadimKalayda <vadosgta1@gmail.com>
2019-12-22 22:40:56 +0200

    added new stats and actions

--- build/js/index.js
+++ build/js/index.js
@@ -2,11 +2,15 @@ let difficult = 0,
   difficultParagraph = document.querySelector('#difficult'),
   easyBtn = document.querySelector('#easy'),
   hardBtn = document.querySelector('#hard'),
+  ninjaBtn = document.querySelector('#ninja'),
   tamagochiName = document.querySelector('#tamagochiName'),
-  myHappiness, myFood, myClean,
+  myHappiness, myFood, myClean, myHP, mySoc, myMoney,
   tamagochiHappiness = document.querySelector('#tamagochiHappiness'),
   tamagochiFood = document.querySelector('#tamagochiFood'),
   tamagochiClean = document.querySelector('#tamagochiClean'),
+  tamagochiHP = document.querySelector('#tamagochiHP'),
+  tamagochiSoc = document.querySelector('#tamagochiSoc'),
+  tamagochiMoney = document.querySelector('#tamagochiMoney'),
   time = 0;
 
 // tamagichi's name
@@ -22,6 +26,11 @@ hardBtn.addEventListener('click', function () {
   difficultParagraph.innerHTML = 'Hard';
 });
 
+ninjaBtn.addEventListener('click', function () {
+  difficult = 2;
+  difficultParagraph.innerHTML = 'Ninja';
+});
+
 //randomize stats
 function randomizeStat(min, max) {
   min = Math.ceil(min);
@@ -29,26 +38,29 @@ function randomizeStat(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-if(difficult === 0) {
-  myHappiness = randomizeStat(50, 100);
-  myFood = randomizeStat(50, 100);
-  myClean = randomizeStat(50, 100);
-} else {
-  myHappiness = randomizeStat(50, 70);
-  myFood = randomizeStat(50, 70);
-  myClean = randomizeStat(50, 70);
-}
+myHappiness = randomizeStat(50, 100);
+myFood = randomizeStat(50, 100);
+myClean = randomizeStat(50, 100);
+myHP = randomizeStat(50, 200);
+mySoc = randomizeStat(50, 100);
+myMoney = randomizeStat(50, 200);
 
+tamagochiHP.innerHTML = myHP;
 tamagochiHappiness.innerHTML = myHappiness;
 tamagochiFood.innerHTML = myFood;
 tamagochiClean.innerHTML = myClean;
+tamagochiSoc.innerHTML = mySoc;
+tamagochiMoney.innerHTML = myMoney;
 
 //Tamagochi Class
 class Tamagochi {
-  constructor(happiness, food, tamagochiClean) {
+  constructor(happiness, food, tamagochiClean, health, soc, money) {
     this.happiness = parseInt(happiness);
     this.food = parseInt(food);
     this.tamagochiClean = parseInt(tamagochiClean);
+    this.health = parseInt(health);
+    this.soc = parseInt(soc);
+    this.money = parseInt(money);
   }
 
   decreaseStats() {
@@ -56,19 +68,36 @@ class Tamagochi {
       this.happiness -= 3;
       this.food -= 3;
       this.tamagochiClean -= 3;
-    } else {
+      this.health -= 3;
+      this.soc -= 3;
+      this.money -= 3;
+    } else if(difficult === 1) {
       this.happiness -= 5;
       this.food -= 5;
       this.tamagochiClean -= 5;
+      this.health -= 5;
+      this.soc -= 5;
+      this.money -= 5;
     }
+  }
+
+  decreaseStats1() {
+    this.happiness -= 7;
+    this.food -= 7;
+    this.tamagochiClean -= 7;
+    this.health -= 7;
+    this.soc -= 7;
+    this.money -= 7;
   }
 
   eat() {
     if(this.food >= 200) {
       this.tamagochiClean -= 20;
+      this.health += 10;
     } else {
       this.food += 30;
       this.tamagochiClean -= 20;
+      this.health += 10;
     }
   }
 
@@ -84,15 +113,78 @@ class Tamagochi {
   myRun() {
     if(this.happiness >= 200) {
       this.food -= 10;
+      this.health -= 5;
     } else {
       this.happiness += 15;
       this.food -= 10;
+      this.health -= 5;
+    }
+  }
+
+  visitDoctor() {
+    if(this.health >= 200) {
+      this.money -= 20;
+    } else {
+      this.health += 30;
+      this.money -= 20;
+    }
+  }
+
+  goToBar() {
+    if(this.soc >= 200) {
+      if(this.food >= 200) {
+        this.money -= 20;
+        this.health -= 10;
+      } else {
+        this.food += 10;
+        this.money -= 20;
+        this.health -= 10;
+      }
+    } else {
+      this.soc += 40;
+      this.food += 10;
+      this.money -= 20;
+      this.health -= 10;
+    }
+  }
+
+  goToWork() {
+    this.money += 50;
+    this.food -= 10;
+    this.health -= 10;
+    this.soc -= 20;
+  }
+
+  buyFood() {
+    if(this.food >= 200) {
+      this.money -= 20;
+    } else {
+      this.food += 20;
+      this.money -= 20;
+    }
+  }
+
+  startBusiness() {
+    if(this.happiness >= 200) {
+      if(this.soc >= 200) {
+        this.money += 100;
+        this.health -= 100;
+      } else {
+        this.money += 100;
+        this.soc += 20;
+        this.health -= 100;
+      }
+    } else {
+      this.money += 100;
+      this.soc += 20;
+      this.happiness += 100;
+      this.health -= 100;
     }
   }
 }
 
 //new Tamagochi
-myTamagochi = new Tamagochi(tamagochiHappiness.innerHTML, tamagochiFood.innerHTML, tamagochiClean.innerHTML);
+myTamagochi = new Tamagochi(tamagochiHappiness.innerHTML, tamagochiFood.innerHTML, tamagochiClean.innerHTML, tamagochiHP.innerHTML, tamagochiSoc.innerHTML, tamagochiMoney.innerHTML);
 console.log(myTamagochi);
 
 //Tamagochi's actions
@@ -106,36 +198,60 @@ console.log(myTamagochi);
 
 (function() {
   setInterval(function () {
-    myTamagochi.decreaseStats();
+    if(difficult === 0 || difficult === 1) {
+      myTamagochi.decreaseStats();
+    } else {
+      myTamagochi.decreaseStats1();
+    }
+    tamagochiHP.innerHTML = myTamagochi.health;
     tamagochiHappiness.innerHTML = myTamagochi.happiness;
     tamagochiFood.innerHTML = myTamagochi.food;
     tamagochiClean.innerHTML = myTamagochi.tamagochiClean;
-    if(myTamagochi.happiness < 0 || myTamagochi.food < 0 || myTamagochi.tamagochiClean < 0) {
+    tamagochiSoc.innerHTML = myTamagochi.soc;
+    tamagochiMoney.innerHTML = myTamagochi.money;
+    if(myTamagochi.food <= 0 || myTamagochi.health <= 0) {
       alert('Oh no!!! Your Tamagochi is dead!!');
       alert('He lived ' + time.toFixed(3) + 's');
       location.reload();
+      return 'dead'
     }
+    if(myTamagochi.happiness <= 0 || myTamagochi.tamagochiClean <= 0 || myTamagochi.soc <= 0) {
+      alert('Oh no!!! Your Tamagochi has ran away!!');
+      alert('He lived ' + time.toFixed(3) + 's');
+      location.reload();
+      return 'ran away'
+    }
+    if(myTamagochi.money <= 0) {
+      alert('Oh no!!! Your Tamagochi doesn\'t have money!!');
+      alert('He lived ' + time.toFixed(3) + 's');
+      location.reload();
+      return 'doesn\'t have money'
+    }
+
   }, 5000)
 })();
 
 document.querySelector('#eat').addEventListener('click', function () {
   myTamagochi.eat();
-  if(myTamagochi.food <= 0 || myTamagochi.tamagochiClean <= 0) {
-    alert('Oh no!!! Your Tamagochi is dead!!');
+  if(myTamagochi.tamagochiClean <= 0) {
+    alert('Oh no!!! Your Tamagochi has ran away!!');
     alert('He lived ' + time.toFixed(3) + 's');
     location.reload();
+    return 'ran away'
   } else {
     tamagochiFood.innerHTML = myTamagochi.food;
     tamagochiClean.innerHTML = myTamagochi.tamagochiClean;
+    tamagochiHP.innerHTML = myTamagochi.health;
   }
 });
 
 document.querySelector('#wash').addEventListener('click', function () {
   myTamagochi.wash();
-  if(myTamagochi.happiness <= 0 || myTamagochi.tamagochiClean <= 0) {
-    alert('Oh no!!! Your Tamagochi is dead!!');
+  if(myTamagochi.happiness <= 0) {
+    alert('Oh no!!! Your Tamagochi has ran away!!');
     alert('He lived ' + time.toFixed(3) + 's');
     location.reload();
+    return 'ran away'
   } else {
     tamagochiHappiness.innerHTML = myTamagochi.happiness;
     tamagochiClean.innerHTML = myTamagochi.tamagochiClean;
@@ -144,12 +260,95 @@ document.querySelector('#wash').addEventListener('click', function () {
 
 document.querySelector('#run').addEventListener('click', function () {
   myTamagochi.myRun();
-  if(myTamagochi.happiness <= 0 || myTamagochi.food <= 0) {
+  if(myTamagochi.food <= 0 || myTamagochi.health <= 0) {
     alert('Oh no!!! Your Tamagochi is dead!!');
     alert('He lived ' + time.toFixed(3) + 's');
     location.reload();
+    return 'dead'
   } else {
     tamagochiHappiness.innerHTML = myTamagochi.happiness;
     tamagochiFood.innerHTML = myTamagochi.food;
+    tamagochiHP.innerHTML = myTamagochi.health;
+  }
+});
+
+document.querySelector('#doctor').addEventListener('click', function () {
+  myTamagochi.visitDoctor();
+  if(myTamagochi.money <= 0) {
+    alert('Oh no!!! Your Tamagochi doesn\'t have money!!');
+    alert('He lived ' + time.toFixed(3) + 's');
+    location.reload();
+    return 'doesn\'t have money'
+  } else {
+    tamagochiHP.innerHTML = myTamagochi.health;
+    tamagochiMoney.innerHTML = myTamagochi.money;
+  }
+});
+
+document.querySelector('#bar').addEventListener('click', function () {
+  myTamagochi.goToBar();
+  if(myTamagochi.health <= 0) {
+    alert('Oh no!!! Your Tamagochi is dead!!');
+    alert('He lived ' + time.toFixed(3) + 's');
+    location.reload();
+    return 'dead'
+  } else if(myTamagochi.money <= 0) {
+    alert('Oh no!!! Your Tamagochi doesn\'t have money!!');
+    alert('He lived ' + time.toFixed(3) + 's');
+    location.reload();
+    return 'doesn\'t have money'
+  } else {
+    tamagochiHP.innerHTML = myTamagochi.health;
+    tamagochiMoney.innerHTML = myTamagochi.money;
+    tamagochiSoc.innerHTML = myTamagochi.soc;
+    tamagochiFood.innerHTML = myTamagochi.food;
+  }
+});
+
+document.querySelector('#work').addEventListener('click', function () {
+  myTamagochi.goToWork();
+  if(myTamagochi.food <= 0 || myTamagochi.health <= 0) {
+    alert('Oh no!!! Your Tamagochi is dead!!');
+    alert('He lived ' + time.toFixed(3) + 's');
+    location.reload();
+    return 'dead'
+  } else if(myTamagochi.soc <= 0) {
+    alert('Oh no!!! Your Tamagochi has ran away!!');
+    alert('He lived ' + time.toFixed(3) + 's');
+    location.reload();
+    return 'ran away'
+  } else {
+    tamagochiHP.innerHTML = myTamagochi.health;
+    tamagochiMoney.innerHTML = myTamagochi.money;
+    tamagochiSoc.innerHTML = myTamagochi.soc;
+    tamagochiFood.innerHTML = myTamagochi.food;
+  }
+});
+
+document.querySelector('#food').addEventListener('click', function () {
+  myTamagochi.buyFood();
+  if(myTamagochi.money <= 0) {
+    alert('Oh no!!! Your Tamagochi doesn\'t have money!!');
+    alert('He lived ' + time.toFixed(3) + 's');
+    location.reload();
+    return 'doesn\'t have money'
+  } else {
+    tamagochiMoney.innerHTML = myTamagochi.money;
+    tamagochiFood.innerHTML = myTamagochi.food;
+  }
+});
+
+document.querySelector('#business').addEventListener('click', function () {
+  myTamagochi.startBusiness();
+  if(myTamagochi.health <= 0) {
+    alert('Oh no!!! Your Tamagochi is dead!!');
+    alert('He lived ' + time.toFixed(3) + 's');
+    location.reload();
+    return 'dead'
+  } else {
+    tamagochiMoney.innerHTML = myTamagochi.money;
+    tamagochiSoc.innerHTML = myTamagochi.soc;
+    tamagochiHP.innerHTML = myTamagochi.health;
+    tamagochiHappiness.innerHTML = myTamagochi.happiness;
   }
 });
